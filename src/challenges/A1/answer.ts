d1864e9abcb0e72fb5361e167ff5c050d042ef6e
axelgourlin <gourlin.axel@gmail.com>
2022-03-15 11:49:07 +0100

    Add A1

--- src/challenges/A1/answer.ts
+++ src/challenges/A1/answer.ts
@@ -1,21 +1,38 @@
 /**
  * In this challenge, you must sort students by their age (younger first). If some of them have
  * the same age, then you should sort them alphabetically (A to Z)
- * 
+ *
  * @param students Unsorted list of students
  * @returns Sorted list of students
  */
 
 // ↓ uncomment bellow lines and add your response!
-/*
-export default function ({ students }: { students: Student[] }): Student[] {
-    return [];
-}
-*/
+
+// export default function ({ students }: { students: Student[] }): Student[] {
+//   return students.sort(
+//     (s1, s2) => s1.age - s2.age || (s1.name > s2.name ? 1 : -1)
+//   );
+//   {
+//     s1.age > s2.age ? 1 : s1.age === s2.age ? (a.name > s2.name ? 1 : -1) : -1
+//     if (s1.age > s2.age) {
+//       return 1;
+//     } else {
+//       if (s1.age === s2.age) {
+//         if (s1.name > s2.name) {
+//           return 1;
+//         } else {
+//           return -1;
+//         }
+//       } else {
+//         return -1;
+//       }
+//     }
+//   });
+// }
 
 // used interfaces, do not touch
 export interface Student {
-    name: string;
-    age: number;
-    skills: string[];
+  name: string;
+  age: number;
+  skills: string[];
 }
